3d632fa1031f6eb2bf1e9f7d7074d3b233d40bd5
nicohman <nico.hickman@gmail.com>
2015-06-23 19:00:19 -0700

    Added randomized UFOS

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ var bullets = [];
 var coolDown = 0;
 var animation = [];
 var frame1;
+var ufos = [];
 var frame2;
 var frame3;
 var score = 0;
@@ -39,6 +40,9 @@ window.onload = function() {
 	body.appendChild(canvas);
 	var loader;
 
+	function randomIntFromInterval(min, max) {
+		return Math.floor(Math.random() * (max - min + 1) + min);
+	}
 
 	var haveCollided = function(rect1, rect2) {
 		if (rect1.topX < rect2.topX + 32 && rect1.topX + 8 > rect2.topX &&
@@ -49,13 +53,14 @@ window.onload = function() {
 			return false;
 		}
 	}
+
 	function changeAudio(sourceUrl) {
-    var audio = $("#player");      
-    $("#player").attr("src", sourceUrl);
-    audio[0].pause();
-    audio[0].load();
-    audio[0].play();
-}
+		var audio = $("#player");
+		$("#player").attr("src", sourceUrl);
+		audio[0].pause();
+		audio[0].load();
+		audio[0].play();
+	}
 	var updateScore = function() {
 		ctx.fillText(score.toString(), 0, 600);
 	}
@@ -110,7 +115,7 @@ window.onload = function() {
 	var tank = imgLoad('./img/tank.png', function() {
 		drawImage(tank, Tank.topX, Tank.topY)
 	});
-
+	ufo = imgLoad('./img/ufo.png')
 	bull = imgLoad('./img/bullet.png', function() {});
 	planeImgs = {
 		'L': imgLoad('./img/plane1.png'),
@@ -119,127 +124,136 @@ window.onload = function() {
 	}
 	var animate = function() {
 
-			ctx.clearRect(0, 0, 500, 610);
-			if (rightDown && Tank.topX + 5 <= 450) {
-				Tank.topX = Tank.topX + 5;
-			} else if (leftDown && Tank.topX - 5 >= 0) {
-				Tank.topX = Tank.topX - 5;
-			}
-			if (spaceDown && bullets.length == 0) {
-				fireBullet();
-				coolDown = 5;
-			} else if (coolDown > 0) {
-				coolDown = coolDown - 1
-			}
-
-			bullets.forEach(function(val, index, arr) {
-				bullets[index].topY = bullets[index].topY - bulletSpeed;
-				drawImage(bull, bullets[index].topX, bullets[index].topY);
-				if (val.topY < 0) {
-					arr.splice(index, 1);
-				}
-			})
-			drawImage(tank, Tank.topX, Tank.topY)
-			if (coolDownL1 < 1) {
-				var wilb;
-				if (Math.round(Math.random() * 1) == 1) {
-					wilb = 1;
-				} else {
-					wilb = -1;
-				}
-				planes.push({
-					topX: 0,
-					orientL: 1,
-					layer: 1,
-					topY: 65
-				})
-				coolDownL1 = 66.66;
-			}
-			if (coolDownL0 < 1) {
-				var wilb;
-				if (Math.round(Math.random() * 1) == 1) {
-					wilb = 1;
-				} else {
-					wilb = -1;
-				}
-				planes.push({
-					topX: 0,
-					orientL: 1,
-					layer: 0,
-					topY: 0
-				})
-				coolDownL0 = 100;
-			}
-			if (coolDownL2 < 1) {
-				var wilb;
-				if (Math.round(Math.random() * 1) == 1) {
-					wilb = 1;
-				} else {
-					wilb = -1;
-				}
-				planes.push({
-					topX: 0,
-					orientL: 1,
-					layer: 2,
-					topY: 130
-				})
-				coolDownL2 = 166.66;
-			}
-			planes.forEach(function(val, index, arr) {
-				var imig;
-				if (planes[index].layer == 2) {
-					planes[index].topX = planes[index].topX + (2.5 * planes[index].orientL);
-
-					imig = planeImgs['B'];
-				} else {
-					planes[index].topX = planes[index].topX + (5 * planes[index].orientL);
-
-					imig = planeImgs['R']
-				}
-				drawImage(imig, planes[index].topX, layas[planes[index].layer])
-
-			});
-			coolDownL1 = coolDownL1 - 1;
-			coolDownL0 = coolDownL0 - 1;
-			coolDownL2 = coolDownL2 - 1;
-			bullets.forEach(function(val, index, arr) {
-				planes.forEach(function(valp, indexp, arrp) {
-					if (haveCollided(val, valp)) {
-						var toAdd;
-						if (valp.layer == 2) {
-							toAdd = 0.5;
-						} else if (valp.layer == 1) {
-							toAdd = 1;
-						} else if (valp.layer == 0) {
-							toAdd = 0;
-						} else {
-							alert('OH NO ITS ALL DIED')
-						}
-						score = score + toAdd;
-						updateScore();
-						animation.push({
-							framenum: 1,
-							topX: valp.topX,
-							topY: valp.topY
-						});
-						bullets.splice(index, 1);
-						planes.splice(indexp, 1);
-					}
-				})
-			})
-			if (animation.length > 0) {
-				animation.forEach(function(val, index, arr) {
-					if (val.framenum > 3) {
-						arr.splice(index, 1);
-						return;
-					}
-					changeAudio('./audio/boom.mp3');
-					drawImage(window['frame' + val.framenum.toString()], val.topX, val.topY)
-					arr[index].framenum = arr[index].framenum + 1
-				});
-			}
-			updateScore();
+		ctx.clearRect(0, 0, 500, 610);
+		if (rightDown && Tank.topX + 5 <= 450) {
+			Tank.topX = Tank.topX + 5;
+		} else if (leftDown && Tank.topX - 5 >= 0) {
+			Tank.topX = Tank.topX - 5;
 		}
+		if (spaceDown && bullets.length == 0) {
+			fireBullet();
+			coolDown = 5;
+		} else if (coolDown > 0) {
+			coolDown = coolDown - 1
+		}
+		if(randomIntFromInterval(0, 667) ===4){
+			alert('UFO!');
+			ufos.push({
+				topY:randomIntFromInterval(140, 200),
+				topX:0
+			});
+		}
+		ufos.forEach(function(val, index, arr){
+			drawImage(ufo, val.topX, val.topY);
+		});
+		bullets.forEach(function(val, index, arr) {
+			bullets[index].topY = bullets[index].topY - bulletSpeed;
+			drawImage(bull, bullets[index].topX, bullets[index].topY);
+			if (val.topY < 0) {
+				arr.splice(index, 1);
+			}
+		})
+		drawImage(tank, Tank.topX, Tank.topY)
+		if (coolDownL1 < 1) {
+			var wilb;
+			if (Math.round(Math.random() * 1) == 1) {
+				wilb = 1;
+			} else {
+				wilb = -1;
+			}
+			planes.push({
+				topX: 0,
+				orientL: 1,
+				layer: 1,
+				topY: 65
+			})
+			coolDownL1 = 66.66;
+		}
+		if (coolDownL0 < 1) {
+			var wilb;
+			if (Math.round(Math.random() * 1) == 1) {
+				wilb = 1;
+			} else {
+				wilb = -1;
+			}
+			planes.push({
+				topX: 0,
+				orientL: 1,
+				layer: 0,
+				topY: 0
+			})
+			coolDownL0 = 100;
+		}
+		if (coolDownL2 < 1) {
+			var wilb;
+			if (Math.round(Math.random() * 1) == 1) {
+				wilb = 1;
+			} else {
+				wilb = -1;
+			}
+			planes.push({
+				topX: 0,
+				orientL: 1,
+				layer: 2,
+				topY: 130
+			})
+			coolDownL2 = 166.66;
+		}
+		planes.forEach(function(val, index, arr) {
+			var imig;
+			if (planes[index].layer == 2) {
+				planes[index].topX = planes[index].topX + (2.5 * planes[index].orientL);
+
+				imig = planeImgs['B'];
+			} else {
+				planes[index].topX = planes[index].topX + (5 * planes[index].orientL);
+
+				imig = planeImgs['R']
+			}
+			drawImage(imig, planes[index].topX, layas[planes[index].layer])
+
+		});
+		coolDownL1 = coolDownL1 - 1;
+		coolDownL0 = coolDownL0 - 1;
+		coolDownL2 = coolDownL2 - 1;
+		bullets.forEach(function(val, index, arr) {
+			planes.forEach(function(valp, indexp, arrp) {
+				if (haveCollided(val, valp)) {
+					var toAdd;
+					if (valp.layer == 2) {
+						toAdd = 0.5;
+					} else if (valp.layer == 1) {
+						toAdd = 1;
+					} else if (valp.layer == 0) {
+						toAdd = 0;
+					} else {
+						alert('OH NO ITS ALL DIED')
+					}
+					score = score + toAdd;
+					updateScore();
+					animation.push({
+						framenum: 1,
+						topX: valp.topX,
+						topY: valp.topY
+					});
+					bullets.splice(index, 1);
+					planes.splice(indexp, 1);
+				}
+			})
+		})
+		if (animation.length > 0) {
+			animation.forEach(function(val, index, arr) {
+				if (val.framenum > 3) {
+					arr.splice(index, 1);
+					return;
+				}
+				changeAudio('./audio/boom.mp3');
+				drawImage(window['frame' + val.framenum.toString()], val.topX, val.topY)
+				arr[index].framenum = arr[index].framenum + 1
+			});
+		}
+		updateScore();
+	}
 
 	setInterval(animate, frameRate);
 }
